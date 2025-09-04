44ad312ce4cc5079ee90d5252091e1af3fe5486c
xaaxdark <127059088+xaaxdark@users.noreply.github.com>
2025-09-04 20:17:59 +0200

    Fix Cloudflare Pages configuration

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  images: {
+    unoptimized: true
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
   env: {
     CLOUDFLARE_ACCOUNT_ID: process.env.CLOUDFLARE_ACCOUNT_ID,
     CLOUDFLARE_DATABASE_ID: process.env.CLOUDFLARE_DATABASE_ID,
     CLOUDFLARE_API_TOKEN: process.env.CLOUDFLARE_API_TOKEN,
     CLOUDFLARE_R2_BUCKET: process.env.CLOUDFLARE_R2_BUCKET,
-  }
-};
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+    ]
+  },
+}
 
 export default nextConfig;
